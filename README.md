a62934ec306c9c7980f3e23a6b568be089edd0d0
Sean Leather <sean.leather@gmail.com>
2017-01-09 14:23:14 +0200

    Add README.md, install.sh

---
+++ README.md
@@ -0,0 +1,8 @@
+# Introduction
+
+This repository contains the configuration files that I (Sean Leather) use for
+`tmux` across various platforms.
+
+# Usage
+
+Run the [`install.sh`](./install.sh) script.
---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# Symbolic link with force and don't follow existing link
+LN="ln -nsf --"
+
+# Full directory path of script
+SRC=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
+
+$LN "$SRC/.tmux.conf" "$HOME/"
